1bccff244226bc484e69e0291d9992d778471f7e
Jan <jansevounek@gmail.com>
2024-09-21 20:28:22 +0200

    .env synced between backend a frontend

--- Learner_frontend/vite.config.js
+++ Learner_frontend/vite.config.js
@@ -3,12 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// .env situation
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  define: {
+    'process.env': process.env
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
