d5ae74e9a5a2e10eb3ecfe15b9ba96493fe84dec
Jan <jansevounek@gmail.com>
2024-12-12 20:56:54 +0100

    tiny tiny progress

--- Learner_frontend/src/components/selector/learning/LessonSolutionsSelector.vue
+++ Learner_frontend/src/components/selector/learning/LessonSolutionsSelector.vue
@@ -10,32 +10,21 @@
             </div>
             <div class="selection-container-addon" @click="currentIndex = 1" :class="{ selected2: currentIndex === 1 }">
                 <div class="mx-2">
-                    <h1 class="selector-title">Settigns</h1>
-                    <p>Network load allowed: {{ lesson.settings.network_load }}%</p>
-                    <p>Cpu load allowed: {{ lesson.settings.cpu_load }}%</p>
-                    <p>Network allowed: {{ lesson.settings.network }}</p>
-                    <p>Sudo: {{ lesson.settings.sudo }}</p>
+                    <h1 class="selector-title">Task</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, tenetur. Enim maiores, necessitatibus sint consequatur placeat ad commodi, fuga harum consectetur, atque dolorum veniam modi impedit laborum! Eos, iusto voluptate!</p>
                 </div>
             </div>
             <div class="selection-container-addon border-b-0 lg:border-b-2 h-16 flex justify-center items-center" @click="currentIndex = 2" :class="{ selected2: currentIndex === 2 }">
-                <button class="selector-addon-button" :class="{ selected_button: currentIndex === 2,}" @click="changeContainer()" v-if="!containerRunning && containerPickedId">Change Container</button>
-                <button class="selector-addon-button" :class="{ selected_button: currentIndex === 2 }" @click="router.push('/learning/admin')" v-if="!containerRunning && !containerPickedId">Exit</button>
-                <button class="selector-addon-button" :class="{ selected_button: currentIndex === 2 }" @click="stopContainer()" v-if="containerRunning && containerPickedId">Stop container</button>
+                <button class="selector-addon-button" :class="{ selected_button: currentIndex === 2 }" @click="router.push('/learning/admin')">Exit</button>
             </div>
         </div>
-        <div class="linux-container" v-if="!containerPickedId">
+        <div class="linux-container">
             <div class="container-list" v-if="containers">
                 <div class="container-list-item" v-for="(container, index) in containers" @click="currentIndex = 3 + index" :class="{ selected2: currentIndex === 3 + index }">
                     <p class="mx-auto">Container code-name: {{ container.name }}</p>
                     <p class="mx-auto">Container author: {{ container.email }}</p>
                     <button class="cmd-input-mobile-btn mb-0" @click="selectContainer()">Check container</button>
                 </div>
-            </div>
-        </div>
-        <div class="linux-container" v-if="containerPickedId" @click="currentIndex = 3" :class="{ selected2: currentIndex === 3 }">
-            <div>
-                <button class="linux-button" :class="{ selected_button: currentIndex === 3 }" @click="useContainer()" v-if="!containerRunning">Check Container</button>
-                <iframe :src="url" width="100%" height="100%" frameborder="0" class="practice-cmd" v-if="containerRunning"></iframe>
             </div>
         </div>
     </div>
@@ -56,11 +45,6 @@ let lastContainerIndex = ref(0)
 
 let lesson = ref()
 let containers = ref([[]])
-
-let containerRunning = ref(false)
-let url = ref('')
-let containerPickedId = ref(false)
-let pickedId = ref(-1)
 
 let listLength = ref(4)
 const horLength = 2
@@ -116,42 +100,21 @@ function handleKeyDown(event) {
     } else if (event.key === "Enter" && currentIndex.value >= 3) {
         selectContainer()
     } else if (event.key === "Enter" && currentIndex.value == 2) {
-        useControlButton()
+        exit()
     }
 }
 
-function useControlButton() {
-    console.log(currentIndex.value)
-    if (!containerRunning.value && containerPickedId.value) {
-        console.log("now")
-        changeContainer()
-    } else if (!containerRunning.value && !containerPickedId.value) {
-        router.push('/learning/admin')
-    } else if (containerRunning.value && containerPickedId.value) {
-        stopContainer()
-    }
-}
-
-function stopContainer() {
-
+function exit() {
+    router.push('/learning/admin')
 }
 
 function selectContainer() {
-    containerPickedId.value = true
-    pickedId.value = currentIndex.value - containers.value.length
-    listLength.value = 4
-}
-
-function changeContainer() {
-    containerPickedId.value = false
-    pickedId.value = -1
-    listLength.value = 3 + containers.value.length
+    console.log("select")
 }
 
 async function setVariables() {
     const data = await getLesson({ id : lessonId })
     lesson.value = data[0]
-    url.value = lesson.value.port
     if (lesson) {
         containers.value = await getContainer({ lesson_id: lessonId })
         listLength.value = 3 + containers.value.length
