b54b8a8bf8fe2aa96c15457487f586c47839beb1
Jan <jansevounek@gmail.com>
2025-01-01 16:37:54 +0100

    Flask nwo deployed with wsgi

--- Learner_backend/Learner_api/app/__init__.py
+++ Learner_backend/Learner_api/app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_supabase import Supabase
-import os
 
 from .routes.teams import UserTeamRoutes, AdminTeamRoutes
 from .routes.lessons import AdminLessonRoutes, UserLessonRoutes
